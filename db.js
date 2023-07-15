02ac51d5c7c60c9dbc8908dc7186cecd0e5caf12
sekar N <sekarnagarajan1234@gmail.com>
2023-07-15 21:16:13 +0530

    js

--- db.js
+++ db.js
@@ -1,3 +1,9 @@
 const mongodb = require("mongodb");
 
-const 
+const MongoClient = mongodb.MongoClient;
+
+let database;
+
+async function getDatabase() {
+  const client = MongoClient.connect("mongodb://127.0.0.1:27017");
+}
